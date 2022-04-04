b77c9afc802ffa0bea69b24e0f43e5964a10734d
minibear2333 <pzqu@qq.com>
2022-04-04 18:17:26 +0800

    fix(handler): 参数错误未按错误码要求返回内容

--- api/internal/handler/interviews/updateinterviewhandler.go
+++ api/internal/handler/interviews/updateinterviewhandler.go
@@ -1,7 +1,9 @@
 package interviews
 
 import (
+	"github.com/minibear2333/programmer-go/api/common/perr"
 	"github.com/minibear2333/programmer-go/api/common/result"
+	"github.com/pkg/errors"
 	"net/http"
 
 	"github.com/minibear2333/programmer-go/api/internal/logic/interviews"
@@ -14,7 +16,8 @@ func UpdateInterviewHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var req types.ReqInterviewUpdate
 		if err := httpx.Parse(r, &req); err != nil {
-			httpx.Error(w, err)
+			err = errors.Wrap(perr.NewErrCode(perr.InvalidParamError), err.Error())
+			result.HttpResult(r, w, nil, err)
 			return
 		}
 
--- api/internal/handler/user/getallstarinterviewshandler.go
+++ api/internal/handler/user/getallstarinterviewshandler.go
@@ -1,7 +1,9 @@
 package user
 
 import (
+	"github.com/minibear2333/programmer-go/api/common/perr"
 	"github.com/minibear2333/programmer-go/api/common/result"
+	"github.com/pkg/errors"
 	"net/http"
 
 	"github.com/minibear2333/programmer-go/api/internal/logic/user"
@@ -14,7 +16,8 @@ func GetAllStarInterviewsHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var req types.ReqStarInterviews
 		if err := httpx.Parse(r, &req); err != nil {
-			httpx.Error(w, err)
+			err = errors.Wrap(perr.NewErrCode(perr.InvalidParamError), err.Error())
+			result.HttpResult(r, w, nil, err)
 			return
 		}
 
